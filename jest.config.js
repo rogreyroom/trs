eaba38c52d9bab148cde8347fbf3e9410ede1c03
Robert Adamczewski <robert@adamczewski.me>
2021-04-24 21:49:01 +0200

    chore: update directory paths

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/', '<rootDir>/coverage', '<rootDir>/dist'],
-  moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src', '<rootDir>/pages'],
+  moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/components', '<rootDir>/pages'],
   moduleNameMapper: {
-    '@src/(.*)': '<rootDir>/src/$1',
+    '@components/(.*)': '<rootDir>/components/$1',
     '@pages/(.*)': '<rootDir>/pages/$1',
     '@styles/(.*)': '<rootDir>/styles/$1',
   },
