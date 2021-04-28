ce652ba9a888fcd123d12ea8eb2cac423fc524c7
Robert Adamczewski <robert@adamczewski.me>
2021-04-28 19:19:27 +0200

    feat: create ees finding functions

--- main/server/db/actions/read.ts
+++ main/server/db/actions/read.ts
@@ -1,12 +1,24 @@
 import AsyncNedb from 'nedb-async';
 import getDbConnection from '../connection';
-import { IUser } from './_types';
+import { IUser, IEes } from './_types';
 
 const readDatabases = {
   GET_USER_BY_NAME: async (name: string): Promise<IUser> => {
     const usersDB: AsyncNedb<IUser> = await getDbConnection('users');
     const userData: IUser = await usersDB.asyncFindOne({ user: name });
     return userData;
+  },
+
+  GET_ALL_EES_DATA: async (): Promise<IEes[]> => {
+    const eesDB: AsyncNedb<IEes> = await getDbConnection('ees');
+    const eesData: IEes[] = await eesDB.asyncFind({});
+    return eesData;
+  },
+
+  GET_EES_DATA_BY_ID: async (id: string): Promise<IEes> => {
+    const eesDB: AsyncNedb<IEes> = await getDbConnection('ees');
+    const eesData: IEes = await eesDB.asyncFindOne({ _id: id });
+    return eesData;
   },
 };
 
