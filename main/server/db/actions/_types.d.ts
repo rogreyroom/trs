6330d57dd8b9a733ff75538e38000ecc8e5fc564
Robert Adamczewski <robert@adamczewski.me>
2021-04-28 19:13:54 +0200

    feat: add ees interface

--- main/server/db/actions/_types.d.ts
+++ main/server/db/actions/_types.d.ts
@@ -5,3 +5,12 @@ export interface IUser {
   fullName: string;
   _id: string;
 }
+export interface IEes {
+  doc: string;
+  type: string;
+  countType: string;
+  symbol: string;
+  percent: string;
+  description: string;
+  _id: string;
+}
