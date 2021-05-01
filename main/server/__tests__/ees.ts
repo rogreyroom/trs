ae1c4aeb088939642cbbc5b516ec7fa4527e3413
Robert Adamczewski <robert@adamczewski.me>
2021-05-01 10:05:10 +0200

    test: create validation and ees update tests

--- main/server/__tests__/ees.ts
+++ main/server/__tests__/ees.ts
@@ -1,5 +1,8 @@
 import { IEes } from '../db/actions/_types';
 import readDatabases from '../db/actions/read';
+import updateDatabases from '../db/actions/update';
+import eesSchema from '../db/schemas/eesSchema';
+import schemaValidator from '../db/schemas/validator';
 
 beforeEach(() => {
   process.env.NODE_ENV = 'test';
@@ -32,5 +35,46 @@ describe('Find ees data by ees id', () => {
     const singleEesData: IEes = await readDatabases.GET_EES_DATA_BY_ID(firstEesDataId);
     expect(singleEesData).not.toBeNull();
     expect(singleEesData).toEqual(expect.objectContaining({ _id: firstEesDataId }));
+  });
+});
+
+describe('Validate data and update ees data by id', () => {
+  const updatedEesData: IEes = {
+    doc: 'ees',
+    type: 'task-oriented',
+    countType: 'auto',
+    symbol: '4HHHH',
+    percent: '250',
+    description: 'pracownik przepracuję dwie soboty w miesiącu lub podczas dwóch weekendów osiągnie stan 12 godzin',
+  };
+
+  it('should return error massage if validation fails', () => {
+    const validation = schemaValidator(eesSchema, { doc: 'fake' });
+    expect(validation.status).toBeFalsy();
+  });
+
+  it('should return value object if validation pass', () => {
+    const validation = schemaValidator(eesSchema, updatedEesData);
+    expect(validation.status).toBeTruthy();
+  });
+
+  it('should return status: false when document not found', async () => {
+    const result = await updateDatabases.UPDATE_EES_DATA_BY_ID('0', updatedEesData);
+    expect(result.status).toBeFalsy();
+    expect(result.value).toEqual('Update failed!');
+  });
+
+  it('should return status: false and validation error value: Doc name is required', async () => {
+    const firstEesDataId = '94JL3S80mEfRsUl1';
+    const result = await updateDatabases.UPDATE_EES_DATA_BY_ID(firstEesDataId, {});
+    expect(result.status).toBeFalsy();
+    expect(result.value).toEqual('Doc name is required');
+  });
+
+  it('should update ees data and return status: true and value: Update successful!', async () => {
+    const firstEesDataId = '94JL3S80mEfRsUl1';
+    const result = await updateDatabases.UPDATE_EES_DATA_BY_ID(firstEesDataId, updatedEesData);
+    expect(result.status).toBeTruthy();
+    expect(result.value).toEqual('Update successful!');
   });
 });
