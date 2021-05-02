5808232802a277df1bef9cc826bc720f5efc53d2
Robert Adamczewski <robert@adamczewski.me>
2021-05-02 14:07:05 +0200

    feat: create add and update responsibilities functions

--- main/server/db/actions/add.ts
+++ main/server/db/actions/add.ts
@@ -1,8 +1,9 @@
 import AsyncNedb from 'nedb-async';
 import getDbConnection from '../connection';
 import holidaysSchema from '../schemas/holidaysSchema';
+import responsibilitiesSchema from '../schemas/responsibilitiesSchema';
 import schemaValidator from '../schemas/validator';
-import { IDbOperationResult, IPublicHolidays } from './_types';
+import { IDbOperationResult, IPublicHolidays, IResponsibilities } from './_types';
 import readDatabases from './read';
 
 const addDatabases = {
@@ -18,6 +19,26 @@ const addDatabases = {
     if (!validation.status) return { ...validation };
     const holidaysDB: AsyncNedb<IPublicHolidays> = await getDbConnection('holidays');
     const addResult = holidaysDB && (await holidaysDB.asyncInsert(holidaysData));
+    return addResult && { status: true, value: 'Adding successful!' };
+  },
+
+  ADD_RESPONSIBILITIES_DATA_BY_EMPLOYEE_ID: async (
+    responsibilitiesData: IResponsibilities
+  ): Promise<IDbOperationResult> => {
+    if (Object.keys(responsibilitiesData).length === 0 && responsibilitiesData.constructor === Object)
+      return { status: false, value: `Data was not specify!` };
+    const { employee } = responsibilitiesData;
+    if (typeof employee === 'undefined') return { status: false, value: `Employee is required!` };
+
+    const getResponsibilities = await readDatabases.GET_RESPONSIBILITIES_DATA_BY_EMPLOYEE_ID(employee);
+    const responsibilitiesExists = getResponsibilities?.employee === employee;
+    if (responsibilitiesExists) return { status: false, value: 'Data exist!' };
+
+    const validation = schemaValidator(responsibilitiesSchema, responsibilitiesData);
+    if (!validation.status) return { ...validation };
+
+    const responsibilitiesDB: AsyncNedb<IResponsibilities> = await getDbConnection('responsibilities');
+    const addResult = responsibilitiesDB && (await responsibilitiesDB.asyncInsert(responsibilitiesData));
     return addResult && { status: true, value: 'Adding successful!' };
   },
 };
--- main/server/db/actions/update.ts
+++ main/server/db/actions/update.ts
@@ -1,8 +1,10 @@
 import AsyncNedb from 'nedb-async';
 import getDbConnection from '../connection';
 import eesSchema from '../schemas/eesSchema';
+import responsibilitiesSchema from '../schemas/responsibilitiesSchema';
 import schemaValidator from '../schemas/validator';
-import { IEes, IDbOperationResult } from './_types';
+import { IEes, IDbOperationResult, IResponsibilities } from './_types';
+import readDatabases from './read';
 
 const updateDatabases = {
   UPDATE_EES_DATA_BY_ID: async (id: string, data: IEes): Promise<IDbOperationResult> => {
@@ -14,6 +16,28 @@ const updateDatabases = {
     const updateResult = eesDB && (await eesDB.asyncUpdate({ _id: id }, { $set: { ...data } }));
     if (updateResult === 0) return { status: false, value: 'Update failed!' };
     return { status: true, value: 'Update successful!' };
+  },
+
+  UPDATE_RESPONSIBILITIES_DATA_BY_EMPLOYEE_ID: async (
+    responsibilitiesData: IResponsibilities
+  ): Promise<IDbOperationResult> => {
+    if (Object.keys(responsibilitiesData).length === 0 && responsibilitiesData.constructor === Object)
+      return { status: false, value: `Data was not specify!` };
+
+    const { employee } = responsibilitiesData;
+    if (typeof employee === 'undefined') return { status: false, value: `Employee is required!` };
+
+    const getResponsibilities = await readDatabases.GET_RESPONSIBILITIES_DATA_BY_EMPLOYEE_ID(employee);
+    const responsibilitiesExists = getResponsibilities?.employee === employee;
+    if (!responsibilitiesExists) return { status: false, value: 'Employee data do not exist!' };
+
+    const validation = schemaValidator(responsibilitiesSchema, responsibilitiesData);
+    if (!validation.status) return { ...validation };
+
+    const responsibilitiesDB: AsyncNedb<IResponsibilities> = await getDbConnection('responsibilities');
+    const addResult =
+      responsibilitiesDB && (await responsibilitiesDB.asyncUpdate({ employee }, { $set: { ...responsibilitiesData } }));
+    return addResult && { status: true, value: 'Update successful!' };
   },
 };
 
