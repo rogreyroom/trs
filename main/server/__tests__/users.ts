42927f496a2be8442b78810a74df28586b79c6e6
Robert Adamczewski <robert@adamczewski.me>
2021-05-17 20:43:02 +0200

    test: change users tests

--- main/server/__tests__/users.ts
+++ main/server/__tests__/users.ts
@@ -5,18 +5,13 @@ beforeEach(() => {
   process.env.NODE_ENV = 'test';
 });
 
-describe('Find user by user name', () => {
-  // For simplicity run ones generalDbAction tests and the this one.
-  // Just to have: name: 'user' === null; name: 'updated user' !== null
+// FIND
+describe('Finding app user)', () => {
+  const userName = 'test';
 
-  it('should to be null if user do not exists', async () => {
-    const userData: IUser = await readDatabases.GET_USER_BY_NAME('user');
-    expect(userData).toBeNull();
-  });
-
-  it('should return object with user "updated test"', async () => {
-    const userData: IUser = await readDatabases.GET_USER_BY_NAME('updated test');
-    expect(userData).not.toBeNull();
-    expect(userData).toEqual(expect.objectContaining({ user: 'updated test' }));
+  it('it should be successful if userData object has doc key equal "users" and user key equal "userName = test"', async () => {
+    const userData = await readDatabases.GET_USER_BY_NAME(userName);
+    expect(userData).toEqual(expect.objectContaining({ doc: 'users' }));
+    expect(userData).toEqual(expect.objectContaining({ user: userName }));
   });
 });
