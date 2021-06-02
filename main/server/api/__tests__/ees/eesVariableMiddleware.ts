a0b8158c94be788be3901c4b2d611e7fa0e61893
Robert Adamczewski <robert@adamczewski.me>
2021-06-02 09:15:41 +0200

    test: create tests to pass ees schema and data to the response validation object

---
+++ main/server/api/__tests__/ees/eesVariableMiddleware.ts
@@ -0,0 +1,50 @@
+import { NextFunction, Request, Response } from 'express';
+import eesSchema from '../../../db/schemas/eesSchema';
+import eesVariableMiddleware from '../../ees/variableMiddleware';
+
+const mocNext: Partial<NextFunction> = jest.fn();
+let mockRequest: Partial<Request>;
+let mockResponse: Partial<Response>;
+
+beforeEach(() => {
+  mockRequest = {
+    body: {
+      ees: {
+        doc: 'ees',
+        type: 'task-oriented',
+        countType: 'auto',
+        symbol: '4H',
+        percent: '25',
+        description: 'Some ees description',
+      },
+    },
+  };
+  mockResponse = {
+    statusCode: 0,
+    validation: {
+      schema: null,
+      data: null,
+    },
+  };
+});
+
+describe('Validate ees', () => {
+  it('should return response statusCode 200 and validation object with schema and data objects', async () => {
+    const expectedStatus = 200;
+    const expectedSchema = eesSchema;
+    const expectedData = {
+      doc: 'ees',
+      type: 'task-oriented',
+      countType: 'auto',
+      symbol: '4H',
+      percent: '25',
+      description: 'Some ees description',
+    };
+
+    await eesVariableMiddleware(mockRequest as Request, mockResponse as Response, mocNext as NextFunction);
+
+    expect(mockResponse.statusCode).toBe(expectedStatus);
+    expect(mockResponse.validation.schema).toEqual(expectedSchema);
+    expect(mockResponse.validation.data).toEqual(expectedData);
+  });
+});
