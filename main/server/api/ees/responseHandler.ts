1081f44c9225046bd9a66364af39c735d869e531
Robert Adamczewski <robert@adamczewski.me>
2021-06-02 09:27:41 +0200

    feat: create the responseHandler function for the ees/ requests

---
+++ main/server/api/ees/responseHandler.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import addDatabases from '../../db/actions/add';
+import readDatabases from '../../db/actions/read';
+
+const eesResponseHandler = async (request: Request, response: Response): Promise<Response> => {
+  const { method, body } = request;
+
+  switch (method) {
+    case 'GET':
+      try {
+        const data = await readDatabases.GET_ALL_EES_DATA();
+        response.statusCode = 200;
+        return response.json({
+          status: true,
+          message: 'Eee data OK.',
+          value: data,
+        });
+      } catch (error) {
+        throw new Error('Internal Sever Error.');
+      }
+    case 'POST':
+      try {
+        const data = await addDatabases.ADD_EES_DATA(body);
+        response.statusCode = 200;
+        return response.json({
+          status: true,
+          message: 'Eee data OK.',
+          value: data,
+        });
+      } catch (error) {
+        throw new Error('Internal Sever Error.');
+      }
+    default:
+      response.setHeader('Allow', ['GET', 'POST']);
+      response.statusCode = 405;
+      return response.json({
+        status: false,
+        message: `Method ${method} not allowed.`,
+      });
+  }
+};
+
+export default eesResponseHandler;
