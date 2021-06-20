54881b7d7a8f09095328b0717e17817db5dfafcf
Robert Adamczewski <robert@adamczewski.me>
2021-06-20 20:39:03 +0200

    feat: create a eesRouter and add get and post routes

---
+++ main/server/api/ees/eesRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import validationMiddleware from '../validationMiddleware';
+import eesResponseHandler from './responseHandler';
+import eesVariableMiddleware from './variableMiddleware';
+
+const eesRouter = Router();
+
+eesRouter.get('/', eesResponseHandler);
+eesRouter.post('/', eesVariableMiddleware, validationMiddleware, eesResponseHandler);
+
+export default eesRouter;
