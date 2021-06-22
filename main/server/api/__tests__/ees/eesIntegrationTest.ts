823760131496be646058566f94b68fd9b0a66671
Robert Adamczewski <robert@adamczewski.me>
2021-06-22 08:39:13 +0200

    test: add integration tests for the api/ees/:symbol routes

--- main/server/api/__tests__/ees/eesIntegrationTest.ts
+++ main/server/api/__tests__/ees/eesIntegrationTest.ts
@@ -27,4 +27,26 @@ describe('Check supertest', () => {
     expect(result.statusCode).toEqual(200);
     expect(result.body.message).toEqual('Eee data OK.');
   });
+
+  it('should get api/ees/:symbol and have a response status code equal 200 and body message equal Eee data OK.', async () => {
+    const theSymbol = '4H';
+    const result = await request(app).get(`/api/ees/${theSymbol}`);
+    expect(result.statusCode).toEqual(200);
+    expect(result.body.message).toEqual('Eee data OK.');
+  });
+
+  it('should put api/ees/:symbol and have a response status code equal 200 and body message equal Eee data OK.', async () => {
+    const theSymbol = '4H';
+    const mockData: IEes = {
+      doc: 'ees',
+      type: 'task-oriented',
+      countType: 'auto',
+      symbol: theSymbol,
+      percent: '125',
+      description: 'pracownik przepracuję dwie soboty w miesiącu lub podczas dwóch weekendów osiągnie stan 12 godzin',
+    };
+    const result = await request(app).put(`/api/ees/${theSymbol}`).send({ ees: mockData });
+    expect(result.statusCode).toEqual(200);
+    expect(result.body.message).toEqual('Eee data OK.');
+  });
 });
