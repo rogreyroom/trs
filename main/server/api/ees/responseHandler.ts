d7fd5eaf3ae1b4a6f27942f3a706df21c7db7ec3
Robert Adamczewski <robert@adamczewski.me>
2021-06-22 07:07:59 +0200

    feat: add the handler to get ees data for a specific symbol, create a handler for the ees update

--- main/server/api/ees/responseHandler.ts
+++ main/server/api/ees/responseHandler.ts
@@ -1,13 +1,29 @@
 import { Request, Response } from 'express';
 import addDatabases from '../../db/actions/add';
 import readDatabases from '../../db/actions/read';
+import updateDatabases from '../../db/actions/update';
 
 const eesResponseHandler = async (request: Request, response: Response): Promise<Response | undefined> => {
-  const { method, body } = request;
+  const { method, body, params } = request;
   const { locals } = response;
+  const hasParams = {}.hasOwnProperty.call(request, 'params');
 
   switch (method) {
     case 'GET':
+      if (hasParams && {}.hasOwnProperty.call(params, 'symbol')) {
+        try {
+          const data = await readDatabases.GET_EES_DATA_BY_SYMBOL(params.symbol);
+          response.statusCode = 200;
+          return response.json({
+            status: true,
+            message: 'Eee data OK.',
+            value: data,
+          });
+        } catch (error) {
+          throw new Error('Internal Sever Error.');
+        }
+      }
+
       try {
         const data = await readDatabases.GET_ALL_EES_DATA();
         response.statusCode = 200;
@@ -23,6 +39,7 @@ const eesResponseHandler = async (request: Request, response: Response): Promise
       if (!locals?.validationResult.status) {
         response.json(locals?.validationResult);
       }
+
       try {
         const data = await addDatabases.ADD_EES_DATA(body);
         response.statusCode = 200;
@@ -34,8 +51,31 @@ const eesResponseHandler = async (request: Request, response: Response): Promise
       } catch (error) {
         throw new Error('Internal Sever Error.');
       }
+    case 'PUT':
+      if (!locals?.validationResult.status) {
+        response.json(locals?.validationResult);
+      }
+
+      if (hasParams && {}.hasOwnProperty.call(params, 'symbol')) {
+        try {
+          const data = await updateDatabases.UPDATE_EES_DATA_BY_SYMBOL(params.symbol, body);
+          response.statusCode = 200;
+          return response.json({
+            status: true,
+            message: 'Eee data OK.',
+            value: data,
+          });
+        } catch (error) {
+          throw new Error('Internal Sever Error.');
+        }
+      }
+      response.statusCode = 400;
+      return response.json({
+        status: false,
+        message: 'Eee symbol not set.',
+      });
     default:
-      response.setHeader('Allow', ['GET', 'POST']);
+      response.setHeader('Allow', ['GET', 'POST', 'PUT']);
       response.statusCode = 405;
       return response.json({
         status: false,
